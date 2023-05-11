49dcfa64ba3adb77bac898006376b27ca3b9e29c
prasu99 <root@ip-172-31-2-213.eu-north-1.compute.internal>
2023-05-11 11:57:23 +0000

    11-05-2023 python programs

---
+++ celsius-fahren.py
@@ -0,0 +1,5 @@
+
+a = input("Celsius = ")
+b = (int(a) * 9/5) + 32 
+print(b)
+
---
+++ positive-negative-zero.py
@@ -0,0 +1,17 @@
+
+a = int(input("a = "))
+if a<0:
+   {
+       print("a is negative")
+   
+   }
+elif a>0:
+   {
+       print("a is positive")
+    }   
+    
+else:
+    {
+        print("a is Zero")
+    }
+
